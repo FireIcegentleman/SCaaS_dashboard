7101e7b257874c5d030c6289dbceb1b9ce811d6a
FireIcegentleman <291628148@qq.com>
2023-01-13 17:45:56 +0800

    The first commit

---
+++ scaas_dashboard/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <el-radio v-model="radio" label="1">备选项</el-radio>
+    <el-radio v-model="radio" label="2">备选项</el-radio>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      radio: '1'
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
